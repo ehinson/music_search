acf3971023beeb8f9c3081e7ca81f8f5566167c1
Erin Hinson <hinson.erin@gmail.com>
2015-01-09 17:35:10 -0500

    Add Cover Template.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :search
 
-  root to: 'search#index'
+  root to: 'search#new'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
