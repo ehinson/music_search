e2f21a4bb8167459de64c5ad2c6043f88eac131c
Erin Hinson <hinson.erin@gmail.com>
2015-01-16 15:39:23 -0500

    Connect to database

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,22 @@
 class SearchController < ApplicationController
   def index
+
   end
 
   def new
+    @artists = Artist.all
+    @tracks = Track.all
+    @albums = Album.all
 
   end
 
   def create
+    @artists = Artist.where(:name => '/params['search']['artist'])
+    @tracks = Track.where(:artist => 'Chris Brown')
+    # @albums = Album.all
+
+
+    # @artist = Artist.where(name: /^"#{params['search']['artist']}"$/)
 
   end
 
