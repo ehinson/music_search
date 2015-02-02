8253a65913ed83e6d570fff7c885ab73d7c8bb36
Erin Hinson <hinson.erin@gmail.com>
2015-02-02 10:48:31 -0500

    ENV variables OMG

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   
   def similar_artist(artist)
-    @similar = JSON.parse(open(URI.encode("http://developer.echonest.com/api/v4/artist/similar?api_key=NSWIO5XPDVJM8EN5F&name=#{artist}&format=json&results=1&start=0")).read)
+    @similar = JSON.parse(open(URI.encode("http://developer.echonest.com/api/v4/artist/similar?api_key=<%= ENV['API_KEY']%>&name=#{artist}&format=json&results=1&start=0")).read)
     @sim_artist = @similar['response']['artists'][0]['name']
     unless @sim.include? @sim_artist
       @sim << @sim_artist
@@ -26,7 +26,7 @@ module ApplicationHelper
 
   def local_artists
     @loc = []
-    @local = JSON.parse(open(URI.encode("http://developer.echonest.com/api/v4/artist/search?api_key=NSWIO5XPDVJM8EN5F&format=json&artist_location=boston&bucket=artist_location")).read)
+    @local = JSON.parse(open(URI.encode("http://developer.echonest.com/api/v4/artist/search?api_key=<%= ENV['API_KEY']%>&format=json&artist_location=boston&bucket=artist_location")).read)
     @loc_artist = @local['response']['artists']
     @loc_artist.each do |a|
       @loc << a['name']
